63fb4a7bb90124af85a1330a726c6d312063d062
ameisso <antoine@antoines-Mac-mini-2.local>
2014-01-29 23:26:39 +0100

    revert

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -9,7 +9,7 @@ void testApp::setup()
     readXmlSetup();
     OscReceiver.setup(oscReceivePort);
     oscSender.setup(oscSendAddress,oscSendPort);
-    profZ=1000; //a modifier, pourra √™tre dans le fichier de config.
+    //profZ=500; //a modifier, pourra être dans le fichier de config.
     for (int i=0; i<nbSynthsForBalls;i++)
     {
         synthsForBalls.push_back(true);
@@ -18,6 +18,7 @@ void testApp::setup()
     lifeSpeed=0.995;
     minDistToLink=100;
     maxDistToUnlink=200;
+    showLinks=true;
 }
 
 //--------------------------------------------------------------
@@ -25,27 +26,30 @@ void testApp::update()
 {
     receiveOscMessage();
     for(vector< ofPtr<Ball> >::iterator it = theBalls.begin(); it != theBalls.end(); ++it)
-        {
-            (*it)->update();
-            sendOscInfos((*it));
-            for(vector< ofPtr<Ball> >::iterator sit = theBalls.begin(); sit != theBalls.end(); ++sit)
-            {
-                if((*sit)->getPosition().distance((*it)->getPosition())<minDistToLink)
-                {
-                    (*it)->addLink((*sit)->getRefNumber());
-                }
-                else if((*it)->checkLink((*sit)->getRefNumber())&&(*sit)->getPosition().distance((*it)->getPosition())>maxDistToUnlink)
-                {
-                    (*it)->removeLink((*sit)->getRefNumber());
-                    //cout<<"link removed ["<<(*sit)->getRefNumber()<<","<<(*it)->getRefNumber()<<"]"<<endl;
-                }
-            }
-            if((*it)->checkIfDead()==true)
-            {
-                 theBalls.erase(it);
-                 break;
-            }
-        }
+	{
+		(*it)->update();
+		sendOscInfos((*it));
+		if(showLinks)
+		{
+			for(vector< ofPtr<Ball> >::iterator sit = theBalls.begin(); sit != theBalls.end(); ++sit)
+			{
+				if((*sit)->getPosition().distance((*it)->getPosition())<minDistToLink)
+				{
+					(*it)->addLink((*sit)->getRefNumber());
+				}
+				else if((*it)->checkLink((*sit)->getRefNumber())&&(*sit)->getPosition().distance((*it)->getPosition())>maxDistToUnlink)
+				{
+					(*it)->removeLink((*sit)->getRefNumber());
+					//cout<<"link removed ["<<(*sit)->getRefNumber()<<","<<(*it)->getRefNumber()<<"]"<<endl;
+				}
+			}
+		}
+		if((*it)->checkIfDead()==true)
+		{
+			theBalls.erase(it);
+			break;
+		}
+	}
 }
 
 //--------------------------------------------------------------
@@ -54,35 +58,37 @@ void testApp::draw()
     ofBackground(0);
     ofSetHexColor(0xffffff);
     for(vector< ofPtr<Ball> >::iterator it = theBalls.begin(); it != theBalls.end(); ++it)
-        {
-            (*it)->draw();
-            for(vector< ofPtr<Ball> >::iterator sit = it; sit != theBalls.end(); ++sit)
-            {
-                if((*it)->checkLink((*sit)->getRefNumber()))
-                {
-                    ofVec3f p1,p2,d,n;
-                    p1=(*it)->getPosition();
-                    p2=(*sit)->getPosition();
-                    d = p2-p1;
-                    n.x = d.y;
-                    n.y = -d.x;
-                    ofPoint cp1 = ofPoint(p1 + 0.4*d+0.2*n);
-                    //ofPoint cp2 = ofPoint(p1+0.9*d+0.1*n);
-                    ofPath link;
-                    ofLine(p1,p1);
-                    link.setFilled(false);
-                    link.quadBezierTo(p1,cp1,p2);
-                    //link.close();
-                    //link.quadBezierTo(p2-0.1*d,cp2,p1+0.1*d);
-                    link.draw();
-
-                }
-            }
-
-        }
+	{
+		(*it)->draw();
+		if(showLinks)
+		{
+			for(vector< ofPtr<Ball> >::iterator sit = it; sit != theBalls.end(); ++sit)
+			{
+				if((*it)->checkLink((*sit)->getRefNumber()))
+				{
+					ofVec3f p1,p2,d,n;
+					p1=(*it)->getPosition();
+					p2=(*sit)->getPosition();
+					d = p2-p1;
+					n.x = d.y;
+					n.y = -d.x;
+					ofPoint cp1 = ofPoint(p1 + 0.4*d+0.2*n);
+					//ofPoint cp2 = ofPoint(p1+0.9*d+0.1*n);
+					ofPath link;
+					ofLine(p1,p1);
+					link.setFilled(false);
+					link.quadBezierTo(p1,cp1,p2);
+					//link.close();
+					//link.quadBezierTo(p2-0.1*d,cp2,p1+0.1*d);
+					link.draw();
+					
+				}
+			}
+		}
+	}
     // we display each ball
-
-
+	
+	
 }
 //--------------------------------------------------------------
 void testApp::keyPressed(int key)
@@ -93,74 +99,80 @@ void testApp::keyPressed(int key)
         cout<<"toggle fullscreen"<<endl;
         ofToggleFullscreen();
     }
-
+	
     if (key == 32) //space to create a ring around the last ball
     {
         if (theBalls.size() != 0)
         {
-             int ringWidth = 5;
+			int ringWidth = 5;
             (*theBalls.back()).addCircle((*theBalls.back()).getRadius(),ringWidth);
         }
     }
     if (key == 114||key==82)// r or R
     {
         cout<<"all ball unloaded"<<endl;
-       theBalls.clear();
+		theBalls.clear();
     }
-
+    if (key == 108||key==76)// l or L
+    {
+        //affiche ou non les liens
+        showLinks=!showLinks;
+    }
+	
 }
 
 //--------------------------------------------------------------
 void testApp::keyReleased(int key)
 {
-
+	
 }
 
 //--------------------------------------------------------------
 void testApp::mouseMoved(int x, int y )
 {
-
+	
 }
 
 //--------------------------------------------------------------
 void testApp::mouseDragged(int x, int y, int button)
 {
-
+	
 }
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button)
 {
     int synthNbr=attributeSynth();
-    cout<<"Synth "<<synthNbr<<endl;
-    cout<<"NF"<<ballNoiseFactor<<endl;
-    cout<<"LS"<<lifeSpeed<<endl;
-    theBalls.push_back(ofPtr<Ball> (new Ball(refNumber,x,y,0,synthNbr,pathToImages,ofRandom(10,50),ballNoiseFactor,lifeSpeed)));
+	/* cout<<"Synth "<<synthNbr<<endl;
+	 cout<<"NF"<<ballNoiseFactor<<endl;
+	 cout<<"LS"<<lifeSpeed<<endl;*/
+    string path=pathToImages+"/"+theTextures[synthNbr];
+    theBalls.push_back(ofPtr<Ball> (new Ball(refNumber,x,y,0,synthNbr,path,ofRandom(10,50),ballNoiseFactor,lifeSpeed)));
     refNumber+=1;
 }
 
 //--------------------------------------------------------------
 void testApp::mouseReleased(int x, int y, int button)
 {
-
+	
 }
 
 //--------------------------------------------------------------
 void testApp::windowResized(int w, int h)
 {
-
+	
 }
 
 //--------------------------------------------------------------
 void testApp::gotMessage(ofMessage msg)
 {
-
+	
 }
 
 //--------------------------------------------------------------
 void testApp::dragEvent(ofDragInfo dragInfo)
 {
-
+	
 }
 void testApp::receiveOscMessage()
 {
@@ -171,52 +183,65 @@ void testApp::receiveOscMessage()
         if(OscReceivedMessage.getAddress()=="/pad/1"||OscReceivedMessage.getAddress()=="/1/multixy1/1")
         {
             //cout<<"first Finger"<<endl;
-            //Si jamais, on pourra toujours atribuer des param√®tres diff√©rents en fonction du nombre de doigts.
+            //Si jamais, on pourra toujours atribuer des paramètres différents en fonction du nombre de doigts.
             float xVal=OscReceivedMessage.getArgAsFloat(0)*ofGetWidth();
             float yVal=OscReceivedMessage.getArgAsFloat(1)*ofGetHeight();
-            cout<<xVal<<" "<<yVal<<endl;
-            theBalls.push_back(ofPtr<Ball> (new Ball (refNumber,xVal,yVal,zPos,attributeSynth(),pathToImages,ofRandom(10,50),ballNoiseFactor,lifeSpeed)));
+            //cout<<xVal<<" "<<yVal<<endl;
+            int synthNbr=attributeSynth();
+            string path=pathToImages+"/"+theTextures[synthNbr];
+            theBalls.push_back(ofPtr<Ball> (new Ball (refNumber,xVal,yVal,zPos,synthNbr,path,ofRandom(10,50),ballNoiseFactor,lifeSpeed)));
             refNumber+=1;
         }
-        else if(OscReceivedMessage.getAddress()=="/pad/2"||OscReceivedMessage.getAddress()=="/1/multixy/2")//deuxi√®me doigt
+        else if(OscReceivedMessage.getAddress()=="/pad/2"||OscReceivedMessage.getAddress()=="/1/multixy/2")//deuxième doigt
         {
-            theBalls.push_back(ofPtr<Ball> (new Ball (refNumber,OscReceivedMessage.getArgAsFloat(0)*ofGetWidth(),OscReceivedMessage.getArgAsFloat(1)*ofGetHeight(),zPos,attributeSynth(),pathToImages,ofRandom(10,50),ballNoiseFactor,lifeSpeed)));
+            int synthNbr=attributeSynth();
+            string path=pathToImages+"/"+theTextures[synthNbr];
+            theBalls.push_back(ofPtr<Ball> (new Ball (refNumber,OscReceivedMessage.getArgAsFloat(0)*ofGetWidth(),OscReceivedMessage.getArgAsFloat(1)*ofGetHeight(),zPos,synthNbr,path,ofRandom(10,50),ballNoiseFactor,lifeSpeed)));
             refNumber+=1;
         }
-        else if(OscReceivedMessage.getAddress()=="/pad/3"||OscReceivedMessage.getAddress()=="/1/multixy/3")//troisi√®me doigt
+        else if(OscReceivedMessage.getAddress()=="/pad/3"||OscReceivedMessage.getAddress()=="/1/multixy/3")//troisième doigt
         {
-            theBalls.push_back(ofPtr<Ball> (new Ball (refNumber,OscReceivedMessage.getArgAsFloat(0)*ofGetWidth(),OscReceivedMessage.getArgAsFloat(1)*ofGetHeight(),zPos,attributeSynth(),pathToImages,ofRandom(10,50),ballNoiseFactor,lifeSpeed)));
+            int synthNbr=attributeSynth();
+            string path=pathToImages+"/"+theTextures[synthNbr];
+            theBalls.push_back(ofPtr<Ball> (new Ball (refNumber,OscReceivedMessage.getArgAsFloat(0)*ofGetWidth(),OscReceivedMessage.getArgAsFloat(1)*ofGetHeight(),zPos,synthNbr,path,ofRandom(10,50),ballNoiseFactor,lifeSpeed)));
             refNumber+=1;
         }
-        else if(OscReceivedMessage.getAddress()=="/pad/4"||OscReceivedMessage.getAddress()=="/1/multixy/4")//quatri√®me doigt
+        else if(OscReceivedMessage.getAddress()=="/pad/4"||OscReceivedMessage.getAddress()=="/1/multixy/4")//quatrième doigt
         {
-            theBalls.push_back(ofPtr<Ball> (new Ball (refNumber,OscReceivedMessage.getArgAsFloat(0)*ofGetWidth(),OscReceivedMessage.getArgAsFloat(1)*ofGetHeight(),zPos,attributeSynth(),pathToImages,ofRandom(10,50),ballNoiseFactor,lifeSpeed)));
+            int synthNbr=attributeSynth();
+            string path=pathToImages+"/"+theTextures[synthNbr];
+            theBalls.push_back(ofPtr<Ball> (new Ball (refNumber,OscReceivedMessage.getArgAsFloat(0)*ofGetWidth(),OscReceivedMessage.getArgAsFloat(1)*ofGetHeight(),zPos,synthNbr,path,ofRandom(10,50),ballNoiseFactor,lifeSpeed)));
             refNumber+=1;
         }
-        else if(OscReceivedMessage.getAddress()=="/pad/5"||OscReceivedMessage.getAddress()=="/1/multixy/5")//cinqui√®me doigt
+        else if(OscReceivedMessage.getAddress()=="/pad/5"||OscReceivedMessage.getAddress()=="/1/multixy/5")//cinquième doigt
         {
-            theBalls.push_back(ofPtr<Ball> (new Ball (refNumber,OscReceivedMessage.getArgAsFloat(0)*ofGetWidth(),OscReceivedMessage.getArgAsFloat(1)*ofGetHeight(),zPos,attributeSynth(),pathToImages,ofRandom(10,50),ballNoiseFactor,lifeSpeed)));
+            int synthNbr=attributeSynth();
+            string path=pathToImages+"/"+theTextures[synthNbr];
+            theBalls.push_back(ofPtr<Ball> (new Ball (refNumber,OscReceivedMessage.getArgAsFloat(0)*ofGetWidth(),OscReceivedMessage.getArgAsFloat(1)*ofGetHeight(),zPos,synthNbr,path,ofRandom(10,50),ballNoiseFactor,lifeSpeed)));
             refNumber+=1;
         }
-        else if (OscReceivedMessage.getAddress()=="/1/multifader1/1")//dur√©e de vie de la balle
+        else if (OscReceivedMessage.getAddress()=="/1/multifader1/1")//durée de vie de la balle
         {
             lifeSpeed=ofMap(OscReceivedMessage.getArgAsFloat(0),0,1,0.9,1);
+            cout<<"lifeSpeed :"<<lifeSpeed<<endl;
         }
-        else if (OscReceivedMessage.getAddress()=="/1/multifader1/2")//vitesse de d√©placement
+        else if (OscReceivedMessage.getAddress()=="/1/multifader1/2")//vitesse de déplacement
         {
-           ballNoiseFactor=(int)200*OscReceivedMessage.getArgAsFloat(0);
+			ballNoiseFactor=(int)200*OscReceivedMessage.getArgAsFloat(0);
+            cout<<"noiseFactor :"<<ballNoiseFactor<<endl;
         }
         else if (OscReceivedMessage.getAddress()=="/1/multifader1/3")//position en z
         {
-           zPos=ofMap(OscReceivedMessage.getArgAsFloat(0),0,1,0,profZ);
+			zPos=ofMap(OscReceivedMessage.getArgAsFloat(0),0,1,0,profZ);
+            cout<<"Zpos :"<<zPos<<endl;
         }
         else
-            {
-                cout<<"I don't know this message :"<<ofToString(OscReceivedMessage.getAddress())<<endl;
-            }
+		{
+			cout<<"I don't know this message :"<<ofToString(OscReceivedMessage.getAddress())<<endl;
+		}
     }
 }
-//fonction qui va envoyer par osc tout les param√®tres de la balle.
+//fonction qui va envoyer par osc tout les paramètres de la balle.
 void testApp::sendOscInfos(ofPtr<Ball>& ballToSend)
 {
     int synthNumber=ballToSend->getSynthNumber();
@@ -229,13 +254,13 @@ void testApp::sendOscInfos(ofPtr<Ball>& ballToSend)
     msgToSend.addFloatArg(ballToSend->getPosition().z);
     msgToSend.addFloatArg(ballToSend->getRadius());
     //-----
-    //ajouter ici les autre param√®tres pour envoyer toutes les infos dans une seule trame ...apr√®s tout l'osc c'est fait pour ca.
+    //ajouter ici les autre paramètres pour envoyer toutes les infos dans une seule trame ...après tout l'osc c'est fait pour ca.
     oscSender.sendMessage(msgToSend);
-   //cout<<"Sended : "<<value<<" @"<< address<<endl;
+	//cout<<"Sended : "<<value<<" @"<< address<<endl;
 }
 int testApp::attributeSynth()
 {
-    //!!! on ne pourra pas cr√©er plus de balles que de synth√©s !
+    //!!! on ne pourra pas créer plus de balles que de synthés !
     int i=ofRandom(nbSynthsForBalls);
     while(synthsForBalls[i]==false)
     {
@@ -265,11 +290,13 @@ void testApp::readXmlSetup()
         OscReceiver.setup(oscReceivePort);
         return;
     }
-
+	
     file.open("config.xml");
     ofBuffer buffer=file.readToBuffer();
     ofXml configFile;
     configFile.loadFromBuffer(buffer.getText());
+    pathToImages=configFile.getValue("path");
+    cout<<"path to Images :"<<pathToImages<<endl;
     configFile.setTo("osc");
     oscReceivePort=configFile.getIntValue("receivePort");
     cout<<"receivePort :"<<oscReceivePort<<endl;
@@ -284,13 +311,17 @@ void testApp::readXmlSetup()
     nbSynthsForBalls=configFile.getIntValue("nbSynthsForBalls");
     cout<<"nbSynthsForBalls :"<<nbSynthsForBalls<<endl;
     configFile.setTo("../textures");
-    pathToImages=configFile.getValue("path");
-    cout<<"path to Images :"<<pathToImages<<endl;
-
+    int nbTextures=configFile.getNumChildren();
+    cout<<"NB textures : "<<nbTextures<<endl;
+    for(int i=0; i<nbTextures; i++)
+    {
+        theTextures.push_back(configFile.getValue("image["+ofToString(i)+"]"));
+        cout<<"texture["<<ofToString(i)<<"]"<<configFile.getValue("image["+ofToString(i)+"]")<<endl;
+    }
+	
     file.close();
     buffer.clear();
     configFile.clear();
     cout<<endl<<"*****************************************************************"<<endl;
     cout<<"XML files read, objects created" <<endl;
 }
-
